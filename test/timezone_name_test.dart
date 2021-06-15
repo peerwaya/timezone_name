26c9850e8bea5dbdb51d154179b8ae4f40bc3206
Onyemaechi Okafor <peerwaya@yahoo.com>
2021-06-15 17:04:33 +0100

    migrate to null safety

--- test/timezone_name_test.dart
+++ test/timezone_name_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:timezone_name/timezone_name.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('timezone_name');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await TimezoneName.platformVersion, '42');
   });
 }
